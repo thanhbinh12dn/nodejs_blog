64beabc94bc49cd9d89f5bf1ca178e0194145350
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2023-04-29 23:48:10 +0700

    CRUD: create, read, update, delete and final is Soft delete

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,69 @@
+const Course = require("../models/Course");
+const { mongooseToObject } = require("../../utils/mongoose");
+
+class CourseController {
+  // [GET] /course/:slug
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then((course) => {
+        res.render("courses/show", { course: mongooseToObject(course) });
+      })
+      .catch(next);
+  }
+
+  // [GET] /course/create
+  create(req, res, next) {
+    res.render("courses/create");
+  }
+
+  // [POST] /course/store
+  store(req, res, next) {
+    req.body.image = `https://i.ytimg.com/vi/${req.body.videoId}/maxresdefault.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch((error) => {});
+  }
+
+  // [GET] /course/:id/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+      .then((course) =>
+        res.render("courses/edit", { course: mongooseToObject(course) })
+      )
+      .catch(next);
+  }
+
+  // [PUT] /course/:id
+  update(req, res, next) {
+    Course.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect("/me/stored/courses"))
+      .catch(next);
+    //redirect giúp điều hướng tới trang theo url đó
+  }
+
+  // [DELETE] /course/:id
+  destroy(req, res, next) {
+    Course.delete({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+    //redirect(back) giúp điều hướng tới trang trước đó
+  }
+
+  // [DELETE] /course/:id/force
+  forceDestroy(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
+
+  // [PATCH] /course/:id/restore
+  restore(req, res, next) {
+    Course.restore({ _id: req.params.id })
+      .then(() => res.redirect("back"))
+      .catch(next);
+  }
+}
+
+module.exports = new CourseController();
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const path = require("path");
 const express = require("express");
+const methodOverride = require("method-override");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 const route = require("./routes");
 const app = express();
 const port = 3000;
+
+const db = require("./config/db");
+
+//connect to db
+db.connect();
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -15,6 +21,8 @@ app.use(
 ); //middleware xu ly du lieu tu form submit len
 app.use(express.json()); // Gui du lieu tu code js
 
+app.use(methodOverride("_method")); //có thêm sự lựa chọn cho form submit
+
 //HTTP logger
 app.use(morgan("combined"));
 
@@ -23,14 +31,17 @@ app.engine(
   "hbs",
   handlebars.engine({
     extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+    },
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 //Routes init
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
