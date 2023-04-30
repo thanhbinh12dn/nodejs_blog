c391e5d66f7827ce54185df6f7e92f513ab202d0
thanhbinh12dn <buithanhbinh.dn12@gmail.com>
2023-04-30 15:53:52 +0700

    Deleted count document

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,13 +4,29 @@ const { multipleMongooseToObject } = require("../../utils/mongoose");
 class MeController {
   // [GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find({})
-      .then((courses) =>
+    Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+      .then(([courses, deletedCount]) => {
         res.render("me/stored-courses", {
+          deletedCount,
           courses: multipleMongooseToObject(courses),
-        })
-      )
+        });
+      })
       .catch(next);
+
+    //2 ông ở dưới này gộp thành promise.all
+    // Course.countDocumentsDeleted()
+    //   .then((deletedCount) => {
+    //     console.log(deletedCount);
+    //   })
+    //   .catch(() => {});
+
+    // Course.find({})
+    //   .then((courses) =>
+    //     res.render("me/stored-courses", {
+    //       courses: multipleMongooseToObject(courses),
+    //     })
+    //   )
+    //   .catch(next);
   }
 
   // [GET] /me/trash/courses
